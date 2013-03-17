9fa7d81af064cc18d972797ac2c85fd746c53d48
link-kut <link.kut@gmail.com>
2013-03-18 05:49:37 +0900

    Create recipe04_2.py

---
+++ recipe04_2.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+import tweepy
+#from recipe__extract_tweet_entities import get_entities
+from recipe03_twitter import get_entities
+
+MAX_PAGES = 15
+RESULTS_PER_PAGE = 100
+
+# Get the trending topics
+
+auth = tweepy.OAuthHandler(consumer_key='WzM7Vji6g7ppvsKhoLj90Q', consumer_secret='YbqAJmmJtzR1uUyQERbhNTlq641DapEDpvuu84B5Co')
+auth.set_access_token(key='225378669-k5IChjgAasI5X8yFDTDIhTFgInLLUzMLFtzelt4D', secret='1SqCwJpFyy710iG7aCJt411LEKSUAttudvKwRv5mcQ')
+
+api = tweepy.API(auth)
+
+WORLD_WOE_ID = 1  #http://woeid.rosselliot.co.nz/lookup
+
+tren = api.trends_place(WORLD_WOE_ID)
+
+
+trends = [ trend['name'] for trend in tren[0]['trends'] ]
+
+idx = 0
+for trend in trends:
+    print '[%i] %s' % (idx, trend,)
+    idx += 1
+
+# Prompt the user
+
+trend_idx = int(raw_input('\nPick a trend: '))
+
+q = trends[trend_idx]
+
+print >> sys.stderr, 'Fetching tweets for %s...' % (q, )
+
+tweepy_search = tweepy.API(parser=tweepy.parsers.JSONParser())
+
+search_results = []
+for page in range(1, MAX_PAGES+1):
+    search_results += \
+        tweepy_search.search(q=q, rpp=RESULTS_PER_PAGE, page=page)['results']
+        
+for result in search_results:
+        result['entities'] = get_entities(result)
+
+if not os.path.isdir('out'):
+        os.mkdir('out')
+
+f = open(os.path.join(os.getcwd(), 'out', 'search_results.json'), 'w')
+f.write(json.dumps(search_results, indent=1))
+f.close()
+
+print >> sys.stderr, "Entities for tweets about trend '%s' saved to %s" % (q, f.name,)
