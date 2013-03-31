caeb14b121381e70f8f092b0f8bdf9e7844776d7
link-kut <link.kut@gmail.com>
2013-03-31 19:53:23 +0900

    Update recipe04_hangul.yhhan.py

--- recipe04_hangul.yhhan.py
+++ recipe04_hangul.yhhan.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
+# tweepy search - http://stackoverflow.com/questions/3256981/tweepy-api-how-to-get-a-users-id-from-a-status-searchresult-object
 
-import os
 import sys
-import json
 import tweepy
-import twitter_text
-
-def get_entities(tweet):                                                                                                                    
-  extractor = twitter_text.Extractor(tweet['text'])                   
-	entities = {}                                                       
-	entities['user_mentions_indices'] = []                              
-	for um in extractor.extract_mentioned_screen_names_with_indices():  
-		entities['user_mentions_indices'].append(um)                      
-                                                                      
-	entities['hashtags_indices'] = []                                   
-	for hts in extractor.extract_hashtags_with_indices():               
-		entities['hashtags_indices'].append(hts)                          
-                                                                      
-	entities['urls_indices'] = []                                       
-	for url in extractor.extract_urls_with_indices():                   
-		entities['urls_indices'].append(url)                              
 
 MAX_PAGES = 10
 RESULTS_PER_PAGE = 2
@@ -37,7 +20,7 @@ trends = [ trend['name'] for trend in tren[0]['trends'] ]
 
 idx = 0
 for trend in trends:
-	print '[%i] %s' % (idx, trend,)
+	print "[%i] %s" % (idx, trend)
 	idx += 1
 
 trend_idx = int(raw_input('\nPick a trend: '))
@@ -46,38 +29,10 @@ q = trends[trend_idx]
 
 print >> sys.stderr, 'Fetching tweets for %s...' % (q, )
 
-tweepy_search = tweepy.API(parser=tweepy.parsers.JSONParser())
-
 search_results = []
 for page in range(1, MAX_PAGES+1):
-	search_results += tweepy_search.search(q=q, rpp=RESULTS_PER_PAGE, page=page)['results']
-        
-for result in search_results:
-	result['entities'] = get_entities(result)
-
-if not os.path.isdir('out'):
-	os.mkdir('out')
-
-#data = json.dumps(search_results, indent=1)
-
-print
+	search_results += api.search(q=q, rpp=RESULTS_PER_PAGE, page=page)
 
 for tweet in search_results:
-	print tweet['text'], '\n'
-
-'''
-f = open(os.path.join(os.getcwd(), 'out', 'search_results.json'), 'w')
-#f = codecs.open(os.path.join(os.getcwd(), 'out', 'search_results.json'), 'w', encoding='utf-8')
-f.write(json.dumps(search_results, indent=1))
-f.close()
-
-print >> sys.stderr, "Entities for tweets about trend '%s' saved to %s" % (q, f.name,)
-print
-print >> sys.stderr, "Read the tweets about trend '%s' from %s" % (q, f.name,)
-
-f = codecs.open(os.path.join(os.getcwd(), 'out', 'search_results.json'), "r", encoding='utf-8')
-search_results2 = f.read()
-f.close()
-
-print search_results2.decode('string_escape')
-'''
+	print tweet
+	print "User '%s(%s)' uploads the tweet:\n%s \n" % (tweet.from_user, tweet.from_user_id, tweet.text)
